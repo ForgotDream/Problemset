a9dc9c6d20b6e91dcc5c7241b4f08b5e6df52ccd
ForgotDream <mr.zyz@outlook.com>
2023-04-24 15:56:22 +0800

    Nothing

--- LOJ/LOJ#10076 Roadblocks.cpp
+++ LOJ/LOJ#10076 Roadblocks.cpp
@@ -1,7 +1,7 @@
 /**
- * @file    
+ * @file    LOJ#10076 Roadblocks.cpp
  * @author  ForgotDream
- * @brief   
+ * @brief   SSSP
  * @date    2023-04-24
  */
 #include <bits/stdc++.h>
@@ -74,19 +74,39 @@ struct Graph {
 
     return dis;
   }
+
+  int twicePath(const int &s, const int &t) {
+    int res = 2e9;
+
+    auto s_dis = dijkstra(s), t_dis = dijkstra(t);
+
+    for (int i = 0; i < edges.size(); i++) {
+      int u = edges[i].u, v = edges[i].v, w = edges[i].w;
+      if (s_dis[u] + t_dis[v] + w <= s_dis[t]) {
+        continue;
+      }
+      res = std::min(res, s_dis[u] + t_dis[v] + w);
+    }
+
+    return res;
+  }
 };
 
 signed main() {
   std::ios::sync_with_stdio(false);
   std::cin.tie(nullptr);
 
-  int n, m;
-  std::cin >> n >> m;
+  int n, r;
+  std::cin >> n >> r;
 
-  std::vector<int> a(n);
-  for (int i = 0; i < n; i++) {
-    std::cin >> a[i];
+  Graph g(n);
+  for (int i = 0; i < r; i++) {
+    int u, v, w;
+    std::cin >> u >> v >> w;
+    g.add(u, v, w);
   }
+
+  std::cout << g.twicePath(1, n) << "\n";
 
   return 0;
 }
