0e050b06c33c15f290cbbf1495b247c15ce02e4e
ForgotDream <mr.zyz@outlook.com>
2023-05-12 08:52:23 +0800

    Nothing

---
+++ Contest/Luogu/进阶 3/A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using i64 = long long;
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  int n, m;
+  std::cin >> n >> m;
+
+  for (int i = 0; i < m; i++) {
+    int u, v;
+    std::cin >> u >> v;
+  }
+
+  return 0;
+}
---
+++ Contest/Luogu/进阶 3/B.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using i64 = long long;
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  int n, m;
+  std::cin >> n >> m;
+
+  std::vector<int> a(n + 1);
+  for (int i = 1; i <= n; i++) {
+    std::cin >> a[i];
+  }
+
+  int lst = 0;
+
+  return 0;
+}
