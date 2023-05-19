80f97455b47b9f3451f941dcad4b87cc5b4fe61f
ForgotDream <mr.zyz@outlook.com>
2023-05-19 17:35:05 +0800

    nothing

--- tmp/tmp.cpp
+++ tmp/tmp.cpp
@@ -2,39 +2,21 @@
  * @file    
  * @author  ForgotDream
  * @brief   
- * @date    2023-05-15
+ * @date    2023-05-19
  */
 #include <bits/stdc++.h>
 
 using i64 = long long;
 
-struct Edge {
-  int u, v;
-  bool deleted;
-  Edge(int _u, int _v) { u = _u, v = _v, deleted = false; }
-  void del() { deleted = true; }
-};
-
 void solve() {
-  int n;
-  std::cin >> n;
-
-  std::vector<Edge> edges;
-  std::vector<std::vector<int>> g(26);
-  for (int i = 0; i < n; i++) {
-    std::string s;
-    std::cin >> s;
-    int u = *s.begin() - 'a', v = *s.rbegin() - 'a';
-    edges.emplace_back(u, v);
-    g[u].push_back(edges.size() - 1);
+  std::array<int, 3> cnt;
+  for (int i = 0; i < 3; i++) {
+    std::cin >> cnt[i];
   }
 
-  std::vector<int> cur(26);
-  std::function<void(int)> dfs = [&](int u) {
-    for (int &i = cur[u]; i < g[u].size(); i++) {
-      
-    }
-  };
+  for (int i = 0; i < 3; i++) {
+    
+  }
 }
 
 signed main() {
@@ -45,7 +27,7 @@ signed main() {
   std::cin >> t;
 
   while (t--) {
-    solve();
+
   }
 
   return 0;
