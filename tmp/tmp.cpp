75c8c768db9fdeb3fb082ef54627e32dbd0de546
ForgotDream <mr.zyz@outlook.com>
2023-06-15 22:30:23 +0800

    230615

--- tmp/tmp.cpp
+++ tmp/tmp.cpp
@@ -8,113 +8,112 @@
 
 using i64 = long long;
 
-struct hashTable {
-  static constexpr int siz = 1e5, mod = 99983;
-  std::vector<int> head, nxt;
-  std::vector<i64> val, stt;
-  int cnt, offset;
-  hashTable() {
-    cnt = 0;
-    head.resize(mod, -1), nxt.resize(siz);
-    stt.resize(siz), val.resize(siz);
-  }
-  void setOffset(int _o) { offset = _o; }
-  void clear() { cnt = 0, head.assign(siz, -1); }
-  int size() { return cnt; }
-  void insert(i64 st, i64 w) {
-    int cur = st % mod;
-    for (int i = head[cur]; ~i; i = nxt[i]) {
-      if (stt[i] == st) {
-        val[i] = std::max(val[i], w);
-        return;
+constexpr int mod = 1e9 + 7;
+
+auto solve(const std::vector<std::vector<int>> &a) {
+  assert(!a.empty());
+  int n = a.size(), m = a[0].size();
+  std::vector f(n, std::vector(m, std::vector<i64>(1 << m)));
+  auto pre = [&](int i, int j, int st) {
+    if (i == 0 && j == 0) {
+      if (st == 0) {
+        return 1ll;
+      } else {
+        return 0ll;
       }
     }
-    stt[cnt] = st, val[cnt] = w;
-    nxt[cnt] = head[cur], head[cur] = cnt++;
+    if (j == 0) {
+      return f[i - 1][m - 1][st];
+    } else {
+      return f[i][j - 1][st];
+    }
+  };
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      for (int st = 0; st < 1 << m; st++) {
+        i64 preVal = pre(i, j, st);
+        if (!a[i][j]) {
+          if ((st >> j) & 1) {
+            f[i][j][st ^ (1 << j)] += preVal;
+          } else {
+            f[i][j][st] += preVal;
+            if (j != m - 1 && !((st >> j) & 3)) {
+              f[i][j][st ^ (1 << (j + 1))] += preVal;
+            }
+            f[i][j][st ^ (1 << j)] += preVal;
+          }
+        } else {
+          f[i][j][st] += preVal;
+        }
+      }
+    }
   }
-  void roll() {
-    for (int i = 0; i < cnt; i++) { stt[i] <<= offset; }
-  }
-};
+  return f;
+}
+
+template<typename T>
+void reverse(std::vector<std::vector<T>> &a) {
+  std::reverse(a.begin(), a.end());
+  for (auto &i : a) { std::reverse(i.begin(), i.end()); }
+}
 
 signed main() {
   std::ios::sync_with_stdio(false);
   std::cin.tie(nullptr);
 
-  int n;
-  std::cin >> n;
+  int n, m;
+  std::cin >> n >> m;
 
-  std::vector a(n, std::vector<int>(n));
-  i64 ans = INT_MIN;
+  std::vector a(n, std::vector<int>(m));
   for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) { 
-      std::cin >> a[i][j]; 
-      ans = std::max(ans, 1ll * a[i][j]); 
-    }
+    for (int j = 0; j < m; j++) { std::cin >> a[i][j]; }
   }
 
-  int offset = 3, mask = (1 << offset) - 1;
-  auto encode = [&](const std::vector<int> &bit, i64 w) {
-    static std::vector<int> b(n + 1);
-    b.assign(n + 1, -1), b[0] = 0;
-    i64 st = 0;
-    int cnt = 1;
-    for (int i = n; i >= 0; i--) {
-      if (!~b[bit[i]]) { b[bit[i]] = cnt++; }
-      st <<= offset, st |= b[bit[i]];
-    }
-    if (cnt <= 2) { ans = std::max(ans, w); }
-    return st;
-  };
-  auto decode = [&](i64 st) {
-    std::vector<int> bit(n + 1);
-    for (int i = 0; i <= n; i++) {
-      bit[i] = st & mask, st >>= offset;
-    }
-    return bit;
-  };
+  auto f = solve(a);
+  reverse(a);
+  auto g = solve(a);
+  reverse(g);
 
-  std::array<hashTable, 2> h;
-  for (int i = 0; i < 2; i++) { h[i].setOffset(offset); }
-
-  int cur = 0, lst = 1;
-
-  auto push = [&](int pl, int dn, int rt, i64 w, const std::vector<int> &bit) {
-    auto b = bit;
-    b[pl] = dn, b[pl + 1] = rt;
-    h[cur].insert(encode(b, w), w);
-  };
-
-  h[cur].insert(0, 0);
   for (int i = 0; i < n; i++) {
-    h[cur].roll();
-    for (int j = 0; j < n; j++) {
-      cur ^= 1, lst ^= 1;
-      h[cur].clear();
-      for (int k = 0; k < h[lst].size(); k++) {
-        i64 w = h[lst].val[k];
-        auto st = decode(h[lst].stt[k]);
-        int lt = st[j], up = st[j + 1];
-        bool rt = (j != n - 1);
-        if (lt && up) {
-          if (lt == up) { push(j, 0, 0, w, st); }
-          for (int l = 0; l <= n; l++) {
-            if (st[l] == lt) { st[l] = up; }
-          }
-          push(j, up, up, w + a[i][j], st);
-        } else if (lt || up) {
-          int t = lt | up;
-          push(j, 0, 0, w, st);
-          push(j, t, t, w + a[i][j], st);
-        } else {
-          push(j, 0, 0, w, st);
-          push(j, n, n, w + a[i][j], st);
-        }
+    for (int j = 0; j < m; j++) { 
+      std::cerr << "[" << i << ", " << j << "]: ";
+      for (int k = 0; k < (1 << m); k++) {
+        std::cerr << f[i][j][k] << " \n"[k == (1 << m) - 1];
+      }
+    }
+  }
+  std::cerr << "\n";
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) { 
+      std::cerr << "[" << i << ", " << j << "]: ";
+      for (int k = 0; k < (1 << m); k++) {
+        std::cerr << g[i][j][k] << " \n"[k == (1 << m) - 1];
       }
     }
   }
 
-  std::cout << ans << "\n";
+  reverse(a);
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      if (a[i][j]) {
+        std::cout << 0 << " \n"[j == m - 1];
+      } else {
+        i64 ans = 0;
+        int prex = (j == 0 ? i - 1 : i), prey = (j == 0 ? m - 1 : j - 1);
+        int nxtx = (j == m - 1 ? i + 1 : i), nxty = (j == m - 1 ? 0 : j + 1);
+        int st = ((1 << m) - 1) ^ (1 << j);
+        for (int k = st; k; k = (k - 1) & st) {
+          i64 pre = (i == 0 && j == 0 ? 1 : f[prex][prey][k]);
+          i64 nxt = (i == n - 1 && j == m - 1 ? 1 : g[nxtx][nxty][k]);
+          (ans += pre * nxt % mod) %= mod;
+        }
+        i64 pre = (i == 0 && j == 0 ? 1 : f[prex][prey][0]);
+        i64 nxt = (i == n - 1 && j == m - 1 ? 1 : g[nxtx][nxty][0]);
+        (ans += pre * nxt % mod) %= mod;
+        std::cout << ans % mod << " \n"[j == m - 1];
+      }
+    }
+  }
 
   return 0;
 }
