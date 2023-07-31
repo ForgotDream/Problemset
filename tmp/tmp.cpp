22f1f5e307798c9c512637f64817bca9e10f26d8
ForgotDream <mr.zyz@outlook.com>
2023-07-31 21:20:22 +0800

    230731

--- tmp/tmp.cpp
+++ tmp/tmp.cpp
@@ -8,30 +8,11 @@
 
 using i64 = long long;
 
-<<<<<<< Updated upstream
-=======
-double pi = std::acos(-1);
-void fft(int *f, int opt) {
-  int len = 1;
-  // perm(len);
-  for (int h = 2; h <= len; h <<= 1) {
-    std::complex<double> delta(std::cos(2 * pi / h), std::sin(opt * 2 * pi / h));
-    for (int k = 0; k < h / 2; k++) {
-      std::complex<double> w(1, 0);
-      for (int j )
-    }
-  }
-}
-
->>>>>>> Stashed changes
 signed main() {
   std::ios::sync_with_stdio(false);
   std::cin.tie(nullptr);
 
-<<<<<<< Updated upstream
   
 
-=======
->>>>>>> Stashed changes
   return 0;
 }
