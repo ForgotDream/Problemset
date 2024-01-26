a7cb9c8b390ca8d2034595c6657e84514a9b9b04
ForgotDream <mr.zyz@outlook.com>
2024-01-26 23:13:55 +0800

    240126

--- tmp/tmp.cpp
+++ tmp/tmp.cpp
@@ -9,7 +9,43 @@
 using i64 = long long;
 using u32 = unsigned;
 
-constexpr int N = 205;
+constexpr int N = 550, inf = 0x3f3f3f3f;
+
+struct Dinic {
+  struct Edge {
+    int u, v, cap;
+  };
+  std::vector<Edge> edges;
+  std::vector<int> adj[N];
+  int cnt, s, t;
+
+  int dis[N];
+  u32 cur[N];
+
+  bool bfs() {
+    std::queue<int> q;
+    memset(dis, 0x3f, sizeof(dis));
+
+    q.push(s), dis[s] = 0;
+    while (!q.empty()) {
+      int u = q.front();
+      q.pop();
+      for (auto i : adj[u]) {
+        auto [_, v, cap] = edges[i];
+        if (dis[v] == inf && cap) {
+          dis[v] = dis[u] + 1;
+          q.push(v);
+        }
+      }
+    }
+
+    return dis[t] != inf;
+  }
+
+  int dfs(int u, int lim) {
+
+  }
+};
 
 void solve() {
 }
